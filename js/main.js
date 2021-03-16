236084bf7ec49270a467bc4aa715cd1768e87b98
Ravena <sabrinagym11@hotmail.com>
2021-03-16 18:24:31 -0400

    Aula finalizada

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
 
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+function redirecionar(){
+    window.open("https://twitter.com/i/timeline");
+    //window.location.href = "https://twitter.com/i/timeline";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+//function soma(n1, n2){
+ //   return n1 + n2;
+//}
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade ?");
+console.log(validaIdade(idade));
+*/
+
+
+
+//alert(soma(5,10));
+
+/*
 var d = new Date();
 alert(d.getMinutes());
 alert(d.getHours());
-
+*/
 
 /*
 var count;
